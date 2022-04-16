3cc86c69c925edd9a5d1d891c46297a55f124c5d
kkkuu-09 <2696455793@qq.com>
2022-04-16 18:14:03 +0800

    photo

--- Sharing/main.go
+++ Sharing/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// 初始化web服务器
-	err := handler.Start(fmt.Sprintf("%s:%s", "127.0.0.1", "8545"), "Static")
+	err := handler.Start(fmt.Sprintf("%s:%s", "127.0.0.1", "8546"), "Static")
 	if err != nil {
 		fmt.Printf("web服务错误:%v\n", err)
 		return
