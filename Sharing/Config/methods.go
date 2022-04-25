f1e1aa847dd7ba97ff16746da146c8b1a7d14e63
HuCuHu <2358804135@qq.com>
2022-04-25 22:00:08 +0800

    修改充值

--- Sharing/Config/methods.go
+++ Sharing/Config/methods.go
@@ -20,7 +20,7 @@ const (
 	//ShareFishAddress = "0x097Dbbe9AC7dDd12919A1C6C1D364852F5bcefdE"
 	//hch
 	Prikey       = "5fd7eb82fbd4c5cc87fbeb867ff4804537723d80a5289606c28ca5c8eed4aa77"
-	ShareFishAddress = "0xD51713a453398A9aE3266695d5F29A659292388c"
+	ShareFishAddress = "0x6fcf9cb4A8E1C39b619b6aeA8d12Bf60313b868f"
 
 	//Prikey       = "45ee84c9fbfee72f88392d5e99fd071bfb1a89c2c1482456547e049d437c1380"
 	//ShareFishAddress = "0x7c095a378463d1B75dDF8A686d7bd7f96ed2276d"
--- Sharing/Handler/topUpEdit.go
+++ Sharing/Handler/topUpEdit.go
@@ -4,6 +4,7 @@ import (
 	config "Sharing/Config"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"math/big"
 	"net/http"
 	"strconv"
@@ -26,9 +27,34 @@ func topUp(c *gin.Context) {
 
 	amount := c.PostForm("amount")
 	fmt.Println("amount",amount)
+	amountf, err := strconv.ParseFloat(amount, 64) //先转换为 float64
+
+	if err != nil {
+
+		log.Println("is not a number")
+
+	}
+
+	// 再通过sprintf格式化为*Int
+
+	valueWei, isOk := new(big.Int).SetString(fmt.Sprintf("%.0f", amountf*1000000000000000000), 10)
+
+	if !isOk {
+		log.Println("float to bigInt failed!")
+	}
+	fmt.Println("wei",valueWei)
+	opts := config.Getopts()
+
+	opts.Value = valueWei
 	amountInt, err := strconv.Atoi(amount)
 	amountInt64 := int64(amountInt)
-	data,err := config.TopUpMethod(client,contract,big.NewInt(amountInt64),LoginUser)
-	fmt.Println("data",data)
+	res, err := contract.TopUp(opts,big.NewInt(amountInt64),LoginUser)
+
+	fmt.Println("topUp:", res)
+	opts.GasLimit = 3000000
+	opts.GasPrice, err = config.GetgasPrice(client)
+	if err != nil {
+		log.Fatal(err)
+	}
 	c.Redirect(http.StatusFound, "/profile")
 }
